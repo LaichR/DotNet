3f391d426560e226b0fcd5563056059ba4e9b14e
LaichR <46196385+LaichR@users.noreply.github.com>
2020-03-22 14:04:13 +0100

    initial version

---
+++ BasicOperations/Operation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOperations
+{
+    public static class Operation
+    {
+        public static double Add(double left, double right)
+        {
+            return left + right;
+        }
+
+        public static double Subtract( double left, double right)
+        {
+            return left - right;
+        }
+
+        public static double Multiply( double left, double right)
+        {
+            return left * right;
+        }
+
+        public static double Power( double left, double right)
+        {
+            return Math.Pow(left, right);
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicOperations;
+
+namespace Calculator
+{
+    class Program
+    {
+        static readonly Dictionary<char, Func<double, double, double>> _operators = new Dictionary<char, Func<double, double, double>>
+        {
+            { '+', Operation.Add },
+            { '-', Operation.Subtract },
+            { '*', Operation.Multiply },
+            { '^', Operation.Power }
+        };
+
+
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                Console.WriteLine("Enter an equation in the form <number ['+'|'-'|'*'|'^'] number> or q to exit:");
+                var input = Console.ReadLine();
+                if( input == "q")
+                {
+                    break;
+                }
+                try
+                {
+                    var operands = input.Split('+', '-', '*', '^');
+                    var @operator = input.Substring(input.IndexOfAny(_operators.Keys.ToArray()), 1);
+                    var operation = _operators[@operator[0]];
+
+                    var left = double.Parse(operands[0]);
+
+                    var right = double.Parse(operands[1]);
+                    Console.WriteLine("Result = {0}", operation(left, right));
+                }
+                catch
+                {
+                    Console.WriteLine("error evaluating arguments");
+                }
+            }
+        }
+    }
+}
